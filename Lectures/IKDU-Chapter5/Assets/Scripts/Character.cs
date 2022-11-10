0564d542deb616a7e9b69075064326f2f0d99332
joshtempest <joshuabussink29@gmail.com>
2022-11-10 11:57:24 +0100

    h

--- Lectures/IKDU-Chapter5/Assets/Scripts/Character.cs
+++ Lectures/IKDU-Chapter5/Assets/Scripts/Character.cs
@@ -11,12 +11,28 @@ public class Character : MonoBehaviour
     {
         chaName = "Name not assigned";
     }
-    public Character(string chaName)
+    public Character(string arg_chaName)
     { 
-        this.chaName = chaName;
+        chaName = arg_chaName;
     }
     public void PrintStatsInfo()
     {
-        Debug.LogFormat("Hero: {0} - {1} attacks", chaName, attack);
+        Debug.LogFormat("Hero: {0} - {1} attacks with", chaName, attack);
+    }
+}
+public struct Weapon
+{
+    public string weaponName;
+    public int damage;
+
+    public Weapon(string arg_name, int arg_damage)
+    {
+        weaponName = arg_name;
+        damage = arg_damage;
+    }
+
+    public void PrintWeaponStats()
+    {
+        Debug.LogFormat("Weapon: {0} - {1} DMG", weaponName, damage);
     }
 }
--- Lectures/IKDU-Chapter5/Assets/Scripts/LearningCurve.cs
+++ Lectures/IKDU-Chapter5/Assets/Scripts/LearningCurve.cs
@@ -11,6 +11,22 @@ public class LearningCurve : MonoBehaviour
         hero.PrintStatsInfo();
         Character heroine = new Character("Agatha");
         heroine.PrintStatsInfo();
+
+        Character hero2 = hero;
+        hero2.PrintStatsInfo();
+        hero2.chaName = "BlahBlah";
+        hero.PrintStatsInfo();
+        hero2.PrintStatsInfo();
+
+        Debug.Log("Here info changes");
+
+        Weapon huntingBow = new Weapon("Hunting bow", 105);
+        huntingBow.PrintWeaponStats();
+        Weapon simpleBow = huntingBow;
+        simpleBow.PrintWeaponStats();
+        simpleBow.weaponName = "trashFlinger";
+        huntingBow.PrintWeaponStats();
+        simpleBow.PrintWeaponStats();
     }
 
     // Update is called once per frame
