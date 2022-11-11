8595860fd98ee5c4826c6bbe0aaf1ac6c8307a6a
joshtempest <joshuabussink29@gmail.com>
2022-11-11 10:53:51 +0100

    k

---
+++ Lectures/IKDU - Hero Born/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Windows;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Vector3 CamOffset = new Vector3(0f, 1.2f, -2.6f);
+    private Transform _target;
+    void Start()
+    {
+        _target = GameObject.Find("Player").transform;
+    }
+    void LateUpdate()
+    {
+        this.transform.position = _target.
+       TransformPoint(CamOffset);
+        this.transform.LookAt(_target);
+    }
+}
